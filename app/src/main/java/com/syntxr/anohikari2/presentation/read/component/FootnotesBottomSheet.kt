7476c888b760f8b1ea17e3041c1ddeabfed130f3
aorync <aserctech@outlook.com>
2024-03-15 11:30:48 +0700

    add footnote

--- app/src/main/java/com/syntxr/anohikari2/presentation/read/component/FootnotesBottomSheet.kt
+++ app/src/main/java/com/syntxr/anohikari2/presentation/read/component/FootnotesBottomSheet.kt
@@ -1,6 +1,8 @@
 package com.syntxr.anohikari2.presentation.read.component
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
@@ -10,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -18,24 +21,25 @@ fun FootNotesBottomSheet(
     hideBottomSheet: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val scrollState = rememberScrollState()
     Column(
         modifier = modifier
     ) {
-        Box(
-            modifier = Modifier.fillMaxWidth()
+        IconButton(
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(8.dp),
+            onClick = { hideBottomSheet() }
         ) {
-            IconButton(
-                modifier = Modifier.align(Alignment.TopEnd).padding(8.dp),
-                onClick = { hideBottomSheet() }
-            ) {
-                Icon(Icons.Default.Close, contentDescription = "Close Footnote")
-            }
+            Icon(Icons.Default.Close, contentDescription = "Close Footnote")
         }
         Spacer(modifier = Modifier.height(24.dp))
         Text(
             text = footNotesContent,
             style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Justify,
             modifier = Modifier.padding(16.dp)
+                .verticalScroll(scrollState)
         )
     }
 }
